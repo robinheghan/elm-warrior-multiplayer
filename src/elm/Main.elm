681b394d79a9d9c60f30b047413ae41dfa848906
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2020-06-03 16:46:14 +0200

    Add walls and items to map.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Player
 import Warrior
+import Warrior.Item as Item
 import Warrior.Map as Map exposing (Map)
 
 
@@ -26,12 +27,31 @@ main =
 
 deathmatch : Map
 deathmatch =
-    Map.init { rows = 12, columns = 12 }
+    Map.init { rows = 12, columns = 13 }
+        |> Map.withWalledArea
+            { x = 6, y = 3 }
+            { x = 6, y = 8 }
         |> Map.withSpawnPoint { x = 2, y = 1 }
         |> Map.withSpawnPoint { x = 2, y = 4 }
         |> Map.withSpawnPoint { x = 2, y = 7 }
         |> Map.withSpawnPoint { x = 2, y = 10 }
-        |> Map.withSpawnPoint { x = 9, y = 1 }
-        |> Map.withSpawnPoint { x = 9, y = 4 }
-        |> Map.withSpawnPoint { x = 9, y = 7 }
-        |> Map.withSpawnPoint { x = 9, y = 10 }
+        |> Map.withSpawnPoint { x = 10, y = 1 }
+        |> Map.withSpawnPoint { x = 10, y = 4 }
+        |> Map.withSpawnPoint { x = 10, y = 7 }
+        |> Map.withSpawnPoint { x = 10, y = 10 }
+        |> Map.withItem { x = 2, y = 0 } Item.Potion
+        |> Map.withItem { x = 2, y = 3 } Item.Potion
+        |> Map.withItem { x = 2, y = 6 } Item.Potion
+        |> Map.withItem { x = 2, y = 9 } Item.Potion
+        |> Map.withItem { x = 10, y = 0 } Item.Potion
+        |> Map.withItem { x = 10, y = 3 } Item.Potion
+        |> Map.withItem { x = 10, y = 6 } Item.Potion
+        |> Map.withItem { x = 10, y = 9 } Item.Potion
+        |> Map.withItem { x = 2, y = 2 } Item.Sword
+        |> Map.withItem { x = 2, y = 5 } Item.Sword
+        |> Map.withItem { x = 2, y = 8 } Item.Sword
+        |> Map.withItem { x = 2, y = 11 } Item.Sword
+        |> Map.withItem { x = 10, y = 2 } Item.Sword
+        |> Map.withItem { x = 10, y = 5 } Item.Sword
+        |> Map.withItem { x = 10, y = 8 } Item.Sword
+        |> Map.withItem { x = 10, y = 11 } Item.Sword
